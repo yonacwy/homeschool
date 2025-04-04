8e90d28455090b3466f01b15503b767bd3332ff1
Yona <3211998+yonacwy@users.noreply.github.com.>
2025-04-04 07:58:00 -0400

    menu

--- app/routes/bible.tsx
+++ app/routes/bible.tsx
@@ -15,21 +15,6 @@ export default function BiblePage() {
 
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-        <h1 className="text-3xl font-bold">
-          <Link to=".">Bible</Link>
-        </h1>
-        <p>{user.email}</p>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
-      </header>
-
       <main className="flex h-full bg-white">
         <div className="h-full w-80 border-r bg-gray-50">
           <hr />
